8958d72d67753a4439a11a2edb44958a0bc3afaf
Farhan <farhan.kzsr23@gmail.com>
2021-10-15 21:55:16 +0700

    update

--- application/controllers/myProfile.php
+++ application/controllers/myProfile.php
@@ -2,7 +2,30 @@
 
 class MyProfile extends CI_Controller{
     function index(){
-        $this->load->view('myprofile-view');
+        $data["quotes"] = "There is no path. Beyond the 
+        scope of light, beyond the reach of Dark... 
+        what could possibly await us? And yet, we seek it, insatiably... Such is our fate.";
+        $data["quotesSource"] = "Aldia, Scholar of the First Sin";
+        $data["about"] = "Hello there, my name is Muhamad Farhan, you can call me Farhan, I born in Palembang on 23 April 2001.
+        Right now I going to State Polytechnic of Sriwijaya majoring in Informatics Management. I do progamming since 2018 
+        starting with C and so on. I have experience with JavaScript, Visual Basic .NET, Dart, PHP, HTML, CSS, SQL, Non SQL, Git, and more,
+        then some framework such as Flutter, Hapi, Node.Js,Express, Laravel, CodeIgniter, Bootstrap, etc. \n 
+        I hope every day is always smooth and without any problems, and for you too of course. \n
+        Here some of my personal project.";
+        $data["projectTitle"] = array(
+            "Meow Book",
+            "Penjualan Sederhana (Simple POS)",
+            "Simple Student Management");
+        $data["projectLink"] = array(
+            "Meonk-Books",
+            "Kasir-Penjualan-Sederhana",
+            "Simple-Manajemen-Siswa"
+        );
+        $data["projectDesc"] = array(
+            "Meow Book is an app that allow you to find, review, search, and read book and novel from Android or iOS. this app built using Flutter", 
+            "Penjualan Sederhana is an app that inspire from complex Point-of-Sales (POS) and we built this with simplicity in mind. this app built using Visual Basic .NET and Ms. Access",
+            "Simple Student Management is an app that to manage student data, using simple (near nonexistent) UI and with help Ms. Access database for ease use. this app built using Visual Basic .NET and Ms. Access");
+        $this->load->view('myprofile-view',$data);
     }
 }
 
--- application/views/myprofile-view.php
+++ application/views/myprofile-view.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Profile</title>
 </head>
-<body class="bg-dark text-white" data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="50" style="background-image: url(<?php echo base_url('assets/img/jumbotron.png');?>); background-size:cover; background-attachment:fixed; background-repeat: no-repeat;" >
-    <nav class="navbar navbar-expand-lg sticky-top navbar-dark rounded" style="background-color: #504f4ffa;">
+<body class="bg-dark text-white" data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="50" style="background-image: url(<?php echo base_url('assets/img/jumbotron.png');?>); background-size:cover; background-attachment:fixed;" >
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #504f4ffa;">
         <div class="container-fluid">
             <a class="navbar-brand" href="#home">FrHaN | Profile</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,61 +32,81 @@
                 Welcome to my profile
             </h2>
             <p class="lead mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis facere pariatur obcaecati voluptatibus et velit aspernatur ratione quas veritatis. Optio eum ab, illum et hic quidem cupiditate architecto aspernatur!
+                <?php echo $quotes?>
             </p>
+            <figcaption class="blockquote-footer mt-1" style="color: white">
+                <?php echo $quotesSource?>
+            </figcaption>
         </div>
     </div>
 
     <div class="d-flex flex-column fs-2 pt-2 bg-dark">
         <h2 class="fs-1 text-center fw-bold text-decoration-underline" id="about">About me</h2>
-        <p class="fs-5 mt-2 mx-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque eveniet recusandae, error nulla odit, culpa ut molestias labore repellendus ea a fugiat est voluptates ad quibusdam corrupti, itaque minima ipsam!</p>
+        <p class="fs-5 mt-2 mx-5">
+        <?php echo $about?>
+        </p>
         <div class="d-flex flex-row justify-content-evenly">
             <div class="diamond-shape">
-                <img src="<?php echo base_url('assets/img/asmbly.png');?>" alt="">
+                <img src="<?php echo base_url('assets/img/javascript.png');?>" alt="">
             </div>
             <div class="diamond-shape">
-                <img src="<?php echo base_url('assets/img/asmbly.png');?>" alt="">
+                <img src="<?php echo base_url('assets/img/flutter.png');?>" alt="">
             </div>
             <div class="diamond-shape">
-                <img src="<?php echo base_url('assets/img/asmbly.png');?>" alt="">
+                <img src="<?php echo base_url('assets/img/html.png');?>" alt="">
             </div>
             <div class="diamond-shape">
-                <img src="<?php echo base_url('assets/img/asmbly.png');?>" alt="">
+                <img src="<?php echo base_url('assets/img/dotnet.png');?>" alt="">
             </div>
         </div>
     </div>
 
+
+
     <div class="d-flex flex-column justify-content-center" style="background-color:#208dbc">
     <div class="Container px-xl-5">
         <div class="container-fluid my-4">
-            <h2 class="fs-1 text-center fw-bold text-decoration-underline pb-4" id="Project">Personal Project</h2>
-            <div class="container-fluid d-flex flex-xl-row justify-content-xl-between flex-column my-xl-5 my-md-4 my-s-5 px-5" style="padding: 0 5vw 0 5vw">
-                <div class ="bloc_1 text-center">
-                    <img src="<?php echo base_url('assets/img/asmbly.png');?>" class="img-thumbnail" alt="" width="320px">
+            <h2 class="fs-1 text-center fw-bold text-decoration-underline pb-4" id="project">Personal project</h2>
+            <?php
+                for($i=0;$i<count($projectTitle);$i++){
+                    if($i%2==0){
+            ?>
+            <div class="container-fluid d-flex flex-xl-row justify-content-xl-between flex-column my-xl-5 my-md-4 my-s-5 px-5 border-bottom" style="padding: 0 5vw 0 5vw">
+                <div class ="bloc_1 text-center mx-5 py-5">
+                    <img src="<?php echo base_url('assets/img/'.$i.'.jpg' );?>" class="img-thumbnail" alt="" width="220px">
                 </div>
                 <div class="bloc_2 my-s-3">
-                    <h2 class="fs-2 text-center pt-3">ABC</h2>
-                     <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro molestiae necessitatibus maxime minima aliquid doloremque voluptatem? Corrupti, labore molestiae est aut exercitationem fuga libero consequuntur ut cupiditate quo cum.
-                     </div>
-                     <div class="d-flex flex-row justify-content-evenly">
-                        <div>abc</div>
-                        <div>abc</div>
-                        <div>abc</div>
-                     </div>
-                </div>
-            </div>
-            <div class="container-fluid d-flex flex-xl-row-reverse justify-content-xl-between flex-column my-3 px-5" style="padding: 0 5vw 0 5vw">
-                <div class ="bloc_1 text-center">
-                    <img src="<?php echo base_url('assets/img/asmbly.png');?>" class="img-thumbnail" alt="" width="320px">
-                </div>
-                <div class="bloc_2">
-                    <h2 class="fs-2 text-center pt-3">ABC</h2>
-                    <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro molestiae necessitatibus maxime minima aliquid doloremque voluptatem? Corrupti, labore molestiae est aut exercitationem fuga libero consequuntur ut cupiditate quo cum.
+                    <h2 class="fs-2 text-center pt-3">
+                        <?php echo $projectTitle[$i] ?>
+                    </h2>
+                    <div class="fs-4 p-xl-4 p-md-3 p-s-2 mx-lg-5 mx-md-3 mx-s-1">
+                        <?php echo $projectDesc[$i] ?>
+                    </div>
+                    <div class="d-flex flex-row justify-content-evenly m-5">
+                        <a href="<?php echo "https://github.com/FrHaN23/".$projectLink[$i]?>" target="_blank" rel="noopener noreferrer">
+                            <img src="<?php echo base_url('assets/ico/github.svg');?>" alt="" width="80vw" style="filter: invert(100%)">
+                        </a>
                     </div>
                 </div>
             </div>
+            <?php } else{?>
+            <div class="container-fluid d-flex flex-xl-row-reverse justify-content-xl-between flex-column my-xl-5 my-md-4 my-s-5 px-5 border-bottom" style="padding: 0 5vw 0 5vw">
+                <div class ="bloc_1 text-center mx-5">
+                    <img src="<?php echo base_url('assets/img/'.$i.'.jpg');?>" class="img-thumbnail" alt="" width="320px">
+                </div>
+                <div class="bloc_2">
+                    <h2 class="fs-2 text-center pt-3"><?php echo $projectTitle[$i] ?></h2>
+                    <div class="fs-4 p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1">
+                    <?php echo $projectDesc[$i] ?>
+                    </div>
+                <div class="d-flex flex-row justify-content-evenly m-5">
+                        <a href="<?php echo "https://github.com/FrHaN23/".$projectLink[$i]?>" target="_blank" rel="noopener noreferrer">
+                            <img src="<?php echo base_url('assets/ico/github.svg');?>" alt="" width="80vw" style="filter: invert(100%)">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <?php } } ?>
         </div>
     </div>
     </div>
@@ -96,40 +116,64 @@
             <h2 class="fs-1 text-center fw-bold text-decoration-underline pb-4" id="contact">
                 Contact me
             </h2>
-            <div class="container-fluid d-flex flex-xl-row justify-content-xl-between flex-column " style="padding: 0 10vw 0 10vw">
-                <div class ="bloc_1 text-center border">
-                    <h2 class="fs-2 text-center pt-3">ABC</h2>
-                    <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1 pb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro molestiae necessitatibus maxime minima aliquid doloremque voluptatem? Corrupti, labore molestiae est aut exercitationem fuga libero consequuntur ut cupiditate quo cum.
+            <div class="container-fluid d-flex flex-xl-row justify-content-xl-center flex-column" style="padding: 0 10vw 0 10vw">
+                <div class ="bloc_1 text-center">
+                    <div class="fs-2 text-center pt-3">
+                        <img src="<?php echo base_url('assets/ico/envelope.svg');?>" alt="" width="60vw" style="filter: invert(100%)">
+                    </div>
+                    <div class="p-xl-4 p-md-3 p-s-2 p-xs-1  mx-md-3 mx-s-1 pb-4">
+                        <a href="mailto:farhan.kzsr23@gmail.com" style="color: inherit;">farhan.kzsr23@gmail.com</a>
                     </div>
                 </div>
-                <div class ="bloc_2 text-center border">
-                    <h2 class="fs-2 text-center pt-3">ABC</h2>
-                    <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1 pb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro molestiae necessitatibus maxime minima aliquid doloremque voluptatem? Corrupti, labore molestiae est aut exercitationem fuga libero consequuntur ut cupiditate quo cum.
+                <div class ="bloc_2 text-center ">
+                    <div class="fs-2 text-center pt-3">
+                        <img src="<?php echo base_url('assets/ico/github.svg');?>" alt="" width="80vw" style="filter: invert(100%)">
+                    </div>
+                    <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-md-3 mx-s-1 pb-4">
+                        <a href="https://github.com/FrHaN23" target="_blank" rel="noopener noreferrer" style="color: inherit;">FrHaN23</a>
                     </div>
                 </div>
             </div>
-            <div class="container-fluid d-flex flex-xl-row justify-content-xl-between flex-column" style="padding: 0 10vw 0 10vw">
-                <div class ="bloc_1 text-center border">
-                    <h2 class="fs-2 text-center pt-3">ABC</h2>
+            <div class="container-fluid d-flex flex-xl-row justify-content-xl-center flex-column" style="padding: 0 10vw 0 10vw">
+                <div class ="bloc_1 text-center">
+                    <div class="fs-2 text-center pt-3">
+                        <img src="<?php echo base_url('assets/ico/telephone.svg');?>" alt="" width="80vw" style="filter: invert(100%)">
+                    </div>
                     <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1 pb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro molestiae necessitatibus maxime minima aliquid doloremque voluptatem? Corrupti, labore molestiae est aut exercitationem fuga libero consequuntur ut cupiditate quo cum.
+                        Undisclosed
                     </div>
                 </div>
-                <div class ="bloc_2 text-center border">
-                    <h2 class="fs-2 text-center pt-3">ABC</h2>
+                <div class ="bloc_2 text-center ">
+                    <div class="fs-2 text-center pt-3">
+                        <img src="<?php echo base_url('assets/ico/house.svg');?>" alt="" width="80vw" style="filter: invert(100%)">
+                    </div>
                     <div class="p-xl-4 p-md-3 p-s-2 p-xs-1 mx-lg-5 mx-md-3 mx-s-1 pb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro molestiae necessitatibus maxime minima aliquid doloremque voluptatem? Corrupti, labore molestiae est aut exercitationem fuga libero consequuntur ut cupiditate quo cum.
+                        Undisclosed
                     </div>
                 </div>
             </div>
         </div>
     </div>
     
-    <footer class="d-flex flex-column fs-2" style="background: rgba(0,0,0,0.87);">
-        <div class="container-fluid">
-            abc
+    <footer class="d-flex flex-column fs-2" style="background: rgba(0, 0, 0, 0.89);">
+        <div class="container-fluid d-flex flex-row justify-content-evenly pt-5 pb-5">
+            <a href="https://web.facebook.com/farhan.kzsr" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo base_url('assets/ico/facebook.svg');?>" alt="" width="50vw" style="filter: invert(100%)">
+            </a>
+            <a href="https://twitter.com/FrHaNkzsr" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo base_url('assets/ico/twitter.svg');?>" alt="" width="50vw" style="filter: invert(100%)">
+            </a>
+            <a href="https://www.instagram.com/frhankzsr/" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo base_url('assets/ico/instagram.svg');?>" alt="" width="50vw" style="filter: invert(100%)">
+            </a>
+            <a href="https://github.com/FrHaN23" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo base_url('assets/ico/github.svg');?>" alt="" width="50vw" style="filter: invert(100%)">
+            </a>
+        </div>
+        <div class="d-flex flex-row justify-content-center my-5">
+            <p class="lead text-center" style="font-size: 15px;">
+                All rights reserved &#169; Muhamad Farhan | Built using <a href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">Bootstrap 5</a> 
+            </p>
         </div>
     </footer>
 
